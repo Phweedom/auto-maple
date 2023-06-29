a17f31220d5206e71348d2d46a49d6a819aa0625
Yijian <30316228+Phweedom@users.noreply.github.com>
2023-06-29 12:54:16 +0800

    update

--- new_resources/command_books/night_lord_2.py
+++ new_resources/command_books/night_lord_2.py
@@ -1,6 +1,6 @@
 from src.common import config, settings, utils
 import time
-from src.routine.components import Command, CustomKey, SkillCombination, Fall, BaseSkill, GoToMap, ChangeChannel, Frenzy, WaitStanding, WealthPotion
+from src.routine.components import Command, CustomKey, SkillCombination, Fall, BaseSkill, GoToMap, ChangeChannel, Frenzy
 from src.common.vkeys import press, key_down, key_up
 import cv2
 
@@ -10,35 +10,29 @@ IMAGE_DIR = config.RESOURCES_DIR + '/command_books/night_lord/'
 class Key:
     # Movement
     JUMP = 'alt'
-    FLASH_JUMP = 'v'
+    FLASH_JUMP = 'alt'
     ROPE = '`'
-    UP_JUMP = 'c' # Shadow Leap
-    DASH = 'x' # Shadow Surge
+    UP_JUMP = 'c'
 
     # Buffs
-    BUFF_F1 = 'f1' # Bleed Dart
-    BUFF_F2 = 'f2' # Maple World Goddess Blessing
-    BUFF_F3 = 'f3' # Throw Blasting
-    BUFF_5 = '5' # Advance Darksight
-    BUFF_F5 = 'f5' # last Resort
-    BUFF_3 = '3' # Soul
-
+    
     # Buffs Toggle
 
     # Attack Skills
-    SKILL_A = 'a' # Showdown
-    SKILL_1 = '1' # Dark lord
-    SKILL_D = 'd' # Dark Flare
-    SKILL_S = 's'# Shurrikane
-    SKILL_W = 'w' # Sudden Raid
-    SKILL_E = 'e' # Death Star
+    SKILL_A = 'a' # 挑釁契約
+    SKILL_1 = '1' # 達克魯的秘傳
+    SKILL_D = 'd' # 絕殺領域
+    SKILL_S = 's'# 風魔手裏劍
+    SKILL_W = 'w' # 穢土轉生
+    SKILL_E = 'e' # 四星鏢雨
+    SKILL_4 = '4' # 飛閃起爆符
     SKILL_F = 'f' # 
-    SKILL_2 = '2' # True Archnid
-    SKILL_4 = 'down+4' # Erda fountain
-
+    SKILL_F2 = 'f2' # 
+    SKILL_2 = '2' # 
+    SKILL_3 = '3' # 
+    SKILL_5 = 'down+5' # 噴泉 fountain
     # special Skills
-    SP_F12 = 'f12' # Totem
-    DARK_EMBRACE = 'shift'
+    SP_F12 = 'f12' # 輪迴
 
 def step(direction, target):
     """
@@ -48,99 +42,65 @@ def step(direction, target):
 
     d_y = target[1] - config.player_pos[1]
     d_x = target[0] - config.player_pos[0]
-    if config.player_states['is_stuck'] and abs(d_x) < 16:
-        print("is stuck")
-        time.sleep(utils.rand_float(0.05, 0.08))
-        x_arrow = ''
-        if direction != 'left' and direction != 'right':
-            if abs(d_x) >= 0:
-                x_arrow = 'right'
-            else:
-                x_arrow = 'left'
-            press(x_arrow+'+'+Key.JUMP)
-        else:
-            press(Key.JUMP)
-        Skill_A(direction='',pre_delay='0.1').execute()
-        WaitStanding(duration='3').execute()
 
     if direction == 'left' or direction == 'right':
-        utils.wait_for_is_standing(2000)
-        d_y = target[1] - config.player_pos[1]
-        d_x = target[0] - config.player_pos[0]
-        
-        if abs(d_x) >= 16:
+        if abs(d_x) >= 18:
             if abs(d_x) >= 60:
                 FlashJump(direction='',triple_jump='true',fast_jump='false').execute()
                 SkillCombination(direction='',jump='false',target_skills='skill_a').execute()
-            elif abs(d_x) >= 28:
+            elif abs(d_x) >= 23:
                 FlashJump(direction='',triple_jump='false',fast_jump='false').execute()
                 SkillCombination(direction='',jump='false',target_skills='skill_a').execute()
             else:
-                if d_y == 0 and Dash().execute():
-                    pass
-                else:
-                    Skill_A(direction='',jump='true').execute()
-            time.sleep(utils.rand_float(0.03, 0.05))
+                SkillCombination(direction='',jump='true',target_skills='skill_a').execute()
+            time.sleep(utils.rand_float(0.05, 0.07))
             if abs(d_x) <= 22:
                 key_up(direction)
             if config.player_states['movement_state'] == config.MOVEMENT_STATE_FALLING:
                 SkillCombination(direction='',jump='false',target_skills='skill_a').execute()
-            utils.wait_for_is_standing(2500)
+            utils.wait_for_is_standing(200)
         else:
-            time.sleep(utils.rand_float(0.1, 0.15))
-            utils.wait_for_is_standing(2500)
+            time.sleep(utils.rand_float(0.08, 0.12))
+            utils.wait_for_is_standing(200)
     
     if direction == 'up':
-        if abs(d_x) > settings.move_tolerance:
-            return
-        utils.wait_for_is_standing(1500)
+        utils.wait_for_is_standing(500)
         if abs(d_y) > 6 :
-            if abs(d_y) < 15:
-                UpJump(pre_delay='0.1').execute()
-            elif abs(d_y) <= 18:
-                UpJump(pre_delay='0.1',jump='true').execute()
+            if abs(d_y) > 36:
+                press(Key.JUMP, 1)
+                time.sleep(utils.rand_float(0.1, 0.15))
+                press(Key.ROPE, 1)
+                time.sleep(utils.rand_float(1.2, 1.5))
+            elif abs(d_y) < 15:
+                UpJump().execute()
+                SkillCombination(direction='',jump='false',target_skills='skill_a|skill_1').execute()
             else:
-                Rope(jump='true').execute()
-            SkillCombination(direction='',jump='false',target_skills='skill_a').execute()
-            time.sleep(utils.rand_float(0.2, 0.3))
-            WaitStanding(duration='3300').execute()
+                press(Key.ROPE, 1)
+                time.sleep(utils.rand_float(1.2, 1.5))
+            utils.wait_for_is_standing(300)
         else:
             press(Key.JUMP, 1)
             time.sleep(utils.rand_float(0.1, 0.15))
 
     if direction == 'down':
-        if abs(d_x) > settings.move_tolerance:
-            return
-        down_duration = 0.2
+        down_duration = 0.04
         if abs(d_y) > 20:
-            down_duration = 0.5
+            down_duration = 0.14
         elif abs(d_y) > 13:
-            down_duration = 0.3
+            down_duration = 0.1
         
         if config.player_states['movement_state'] == config.MOVEMENT_STATE_STANDING and config.player_states['in_bottom_platform'] == False:
             print("down stair")
-            if abs(d_x) >= 15:
+            if abs(d_x) >= 2:
                 if d_x > 0:
                     Fall(direction='right',duration=down_duration).execute()
                 else:
                     Fall(direction='left',duration=down_duration).execute()
-                
             else:
-                Fall(direction='',duration=(down_duration+0.1)).execute()
-                if config.player_states['movement_state'] == config.MOVEMENT_STATE_STANDING:
-                    print("leave lader")
-                    if d_x > 0:
-                        key_down('left')
-                        press(Key.JUMP)
-                        key_up('left')
-                    else:
-                        key_down('right')
-                        press(Key.JUMP)
-                        key_up('right')
+                Fall(direction='',duration=down_duration).execute()
             SkillCombination(direction='',jump='false',target_skills='skill_a').execute()
-                
-        utils.wait_for_is_standing(2000)
-        time.sleep(utils.rand_float(0.1, 0.12))
+        time.sleep(utils.rand_float(0.02, 0.05))
+        utils.wait_for_is_standing(300)
 
 class Adjust(Command):
     """Fine-tunes player position using small movements."""
@@ -216,6 +176,7 @@ class Buff(Command):
         if self.cd180_buff_time == 0 or now - self.cd150_buff_time > 151:
             self.cd150_buff_time = now
         if self.cd180_buff_time == 0 or now - self.cd180_buff_time > 181:
+            Skill_4().execute()
             self.cd180_buff_time = now
         if self.cd200_buff_time == 0 or now - self.cd200_buff_time > 200:
             self.cd200_buff_time = now
@@ -230,60 +191,92 @@ class Buff(Command):
 
 class FlashJump(Command):
     """Performs a flash jump in the given direction."""
-    _display_name = 'FlashJump'
+    _display_name = '二段跳'
 
-    def __init__(self, direction="",jump='false',combo='False',triple_jump="False",fast_jump="false",reverse_triple='false'):
+    def __init__(self, direction="",jump='false',combo='False',triple_jump="False",fast_jump="false"):
         super().__init__(locals())
         self.direction = settings.validate_arrows(direction)
         self.triple_jump = settings.validate_boolean(triple_jump)
         self.fast_jump = settings.validate_boolean(fast_jump)
         self.jump = settings.validate_boolean(jump)
-        self.reverse_triple = settings.validate_boolean(reverse_triple)
 
     def main(self):
         if not self.jump:
-            utils.wait_for_is_standing()
-            if not self.fast_jump:
-                self.player_jump(self.direction)
-                time.sleep(utils.rand_float(0.02, 0.04)) # fast flash jump gap
-            else:
-                key_down(self.direction,down_time=0.05)
-                press(Key.JUMP,down_time=0.06,up_time=0.05)
+            self.player_jump(self.direction)
         else:
             key_down(self.direction,down_time=0.05)
-            press(Key.JUMP,down_time=0.06,up_time=0.05)
-        
-        press(Key.FLASH_JUMP, 1,down_time=0.06,up_time=0.01)
+            press(Key.JUMP,down_time=0.04,up_time=0.02)
+        if not self.fast_jump:
+            time.sleep(utils.rand_float(0.02, 0.04)) # fast flash jump gap
+        else:
+            time.sleep(utils.rand_float(0.2, 0.25)) # slow flash jump gap
+        if self.direction == 'up':
+            press(Key.FLASH_JUMP, 1)
+        else:
+            press(Key.FLASH_JUMP, 1,down_time=0.06,up_time=0.05)
+            if self.triple_jump:
+                time.sleep(utils.rand_float(0.05, 0.08))
+                press(Key.FLASH_JUMP, 1,down_time=0.07,up_time=0.04) # if this job can do triple jump
         key_up(self.direction,up_time=0.01)
-        if self.triple_jump:
-            time.sleep(utils.rand_float(0.03, 0.05))
-            # reverse_direction
-            reverse_direction = ''
-            if self.reverse_triple:
-                if self.direction == 'left':
-                    reverse_direction = 'right'
-                elif self.direction == 'right':
-                    reverse_direction = 'left'
-                print('reverse_direction : ',reverse_direction)
-                key_down(reverse_direction,down_time=0.05)
-            else:
-                time.sleep(utils.rand_float(0.02, 0.03))
-            press(Key.FLASH_JUMP, 1,down_time=0.07,up_time=0.04) # if this job can do triple jump
-            if self.reverse_triple:
-                key_up(reverse_direction,up_time=0.01)
-        time.sleep(utils.rand_float(0.01, 0.02))
+        time.sleep(utils.rand_float(0.02, 0.04))
+
+class FaceLeft(BaseSkill):
+    """Performs a flash jump in the given direction."""
+    _display_name = 'FaceLeft'
+    _distance = 1
+    key='left'
+    delay=0.1
+    rep_interval=0
+    skill_cool_down=0
+    ground_skill=False
+    buff_time=0
+    combo_delay = 0.0
+
+class FaceRight(BaseSkill):
+    """Performs a flash jump in the given direction."""
+    _display_name = 'FaceRight'
+    _distance = 1
+    key='right'
+    delay=0.1
+    rep_interval=0
+    skill_cool_down=0
+    ground_skill=False
+    buff_time=0
+    combo_delay = 0.0
+
+class PressUp(BaseSkill):
+    """Performs a flash jump in the given direction."""
+    _display_name = 'PressUp'
+    _distance = 1
+    key='up'
+    delay=0.25
+    rep_interval=0
+    skill_cool_down=0
+    ground_skill=False
+    buff_time=0
+    combo_delay = 0.0
+
+class FJSD(Command):
+    """Performs a flash jump in the given direction."""
+    _display_name = 'FJSD'
+
+    def main(self):
+        press(Key.JUMP,down_time=0.04,up_time=0.02)
+        press(Key.FLASH_JUMP, 1,down_time=0.06,up_time=0.05)
+        press(Key.SKILL_A, 2)
+        time.sleep(0.3)
 
 class UpJump(BaseSkill):
     """Performs a up jump in the given direction."""
-    _display_name = 'UpJump'
+    _display_name = '上跳'
     _distance = 27
     key=Key.UP_JUMP
-    delay=0.45
+    delay=0.4
     rep_interval=0.5
     skill_cool_down=0
     ground_skill=False
     buff_time=0
-    combo_delay = 0.45
+    combo_delay = 0.4
 
     # def __init__(self,jump='false', direction='',combo='true'):
     #     super().__init__(locals())
@@ -300,7 +293,7 @@ class UpJump(BaseSkill):
         
 class Rope(BaseSkill):
     """Performs a up jump in the given direction."""
-    _display_name = 'Rope'
+    _display_name = '連接繩索'
     _distance = 27
     key=Key.ROPE
     delay=1.4
@@ -310,34 +303,22 @@ class Rope(BaseSkill):
     buff_time=0
     combo_delay = 0.2
 
-class Dash(BaseSkill):
-    """Performs a dash in the given direction."""
-    _display_name = 'Shadow Surge'
-    _distance = 10
-    key=Key.DASH
-    delay=0.25
-    rep_interval=0.5
-    skill_cool_down=5
-    ground_skill=False
-    buff_time=0
-    combo_delay = 0.25
-
 class Skill_A(BaseSkill):
-    _display_name = 'Showdown'
+    _display_name = '挑釁契約'
     _distance = 27
     key=Key.SKILL_A
-    delay=0.52
+    delay=0.5
     rep_interval=0.5
     skill_cool_down=0
     ground_skill=False
     buff_time=0
-    combo_delay = 0.2
+    combo_delay = 0.5
 
 class Skill_1(BaseSkill):
-    _display_name = 'Dark lord'
+    _display_name = '達克魯的秘傳'
     _distance = 27
     key=Key.SKILL_1
-    delay=0.55
+    delay=0.45
     rep_interval=0.5
     skill_cool_down=57
     ground_skill=True
@@ -345,10 +326,10 @@ class Skill_1(BaseSkill):
     combo_delay = 0.5
 
 class Skill_D(BaseSkill):
-    _display_name = 'Dark Flare'
+    _display_name = '絕殺領域'
     _distance = 27
     key=Key.SKILL_D
-    delay=0.55
+    delay=0.45
     rep_interval=0.5
     skill_cool_down=57
     ground_skill=True
@@ -356,132 +337,70 @@ class Skill_D(BaseSkill):
     combo_delay = 0.5
 
 class Skill_S(BaseSkill):
-    _display_name = 'Shurrikane'
+    _display_name = '風魔手裏劍'
     _distance = 50
     key=Key.SKILL_S
-    delay=0.58
+    delay=0.45
     rep_interval=0.5
     skill_cool_down=24
     ground_skill=False
     buff_time=0
-    combo_delay = 0.2
+    combo_delay = 0.45
 
 class Skill_W(BaseSkill):
-    _display_name = 'Sudden Raid'
+    _display_name = '穢土轉生'
     _distance = 50
     key=Key.SKILL_W
-    delay=0.65
+    delay=0.63
     rep_interval=0.5
     skill_cool_down=29
     ground_skill=False
     buff_time=0
-    combo_delay = 0.2
+    combo_delay = 0.63
 
 class Skill_E(BaseSkill):
-    _display_name = 'Death Star'
+    _display_name = '四星鏢雨'
     _distance = 50
     key=Key.SKILL_E
-    delay=0.94
+    delay=0.68
     rep_interval=0.5
     skill_cool_down=14
     ground_skill=False
     buff_time=0
-    combo_delay = 0.2
+    combo_delay = 0.68
 
 class Skill_2(BaseSkill):
-    _display_name ='True Archnid'
+    _display_name ='蜘蛛之鏡'
     key=Key.SKILL_2
-    delay=0.75
+    delay=0.7
     rep_interval=0.25
     skill_cool_down=240
     ground_skill=False
     buff_time=0
     combo_delay = 0.4
 
-class Buff_F1(BaseSkill):
-    _display_name ='Bleed Dart'
-    key=Key.BUFF_F1
-    delay=0.85
-    rep_interval=0.25
-    skill_cool_down=171
-    ground_skill=True
-    buff_time=80
-    combo_delay = 0.8
-
-class Buff_F2(BaseSkill):
-    _display_name ='Maple World Goddess Blessing'
-    key=Key.BUFF_F2
-    delay=0.85
-    rep_interval=0.25
-    skill_cool_down=180
-    ground_skill=True
-    buff_time=60
-    combo_delay = 0.85
-
-class Buff_F3(BaseSkill):
-    _display_name ='Throw Blasting'
-    key=Key.BUFF_F3
-    delay=0.9
+class Skill_4(BaseSkill):
+    _display_name ='飛閃起爆符'
+    key=Key.SKILL_4
+    delay=0.7
     rep_interval=0.25
     skill_cool_down=171
     ground_skill=True
     buff_time=90
     combo_delay = 0.4
 
-class Buff_5(BaseSkill):
-    _display_name ='Advance Darksight'
-    key=Key.BUFF_5
-    delay=0.7
-    rep_interval=0.25
-    skill_cool_down=192
-    ground_skill=True
-    buff_time=30
-    combo_delay = 0.6
-
-class Buff_F5(BaseSkill):
-    _display_name ='last Resort'
-    key=Key.BUFF_F5
-    delay=0.7
-    rep_interval=0.25
-    skill_cool_down=77
-    ground_skill=True
-    buff_time=30
-    combo_delay = 0.7
-
-class Buff_3(BaseSkill):
-    _display_name ='Soul'
-    key=Key.BUFF_3
-    delay=1.45
-    rep_interval=0.25
-    skill_cool_down=143
-    ground_skill=True
-    buff_time=60
-    combo_delay = 0.2
-    skill_image = IMAGE_DIR + 'buff_3.png'
-    active_if_not_in_skill_buff = 'buff_3'
-
-class Skill_4(BaseSkill):
-    _display_name ='Erda fountain'
-    key=Key.SKILL_4
-    delay=0.6
+class Skill_5(BaseSkill):
+    _display_name ='噴泉'
+    key=Key.SKILL_5
+    delay=0.9
     rep_interval=0.25
     skill_cool_down=57
     ground_skill=True
     buff_time=60
     combo_delay = 0.3
 
-class DarkEmbrace(BaseSkill):
-    _display_name ='Dark Embrace'
-    key=Key.DARK_EMBRACE
-    delay=1
-    rep_interval=0.12
-    skill_cool_down=3
-    ground_skill=False
-    buff_time=0
-    combo_delay = 0.1
-
 class AutoHunting(Command):
-    _display_name ='Autohunting'
+    _display_name ='自動走位狩獵'
 
     def __init__(self,duration='180',map=''):
         super().__init__(locals())
@@ -509,7 +428,7 @@ class AutoHunting(Command):
                 ChangeChannel(max_rand=40).execute()
                 Skill_A().execute()
                 continue
-            
+            Frenzy().execute()
             frame = config.capture.frame
             point = utils.single_match_with_threshold(frame,daily_complete_template,0.9)
             if len(point) > 0:
@@ -522,8 +441,6 @@ class AutoHunting(Command):
             if not config.enabled:
                 break
             
-            Frenzy().execute()
-            SkillCombination(target_skills='buff_f3|buff_3|buff_f2').execute()
             if toggle:
                 # right side
                 move((width-20),bottom_y).execute()
@@ -546,7 +463,7 @@ class AutoHunting(Command):
                 print("current bottom : ", settings.platforms)
                 FlashJump(direction='right').execute()
                 Rope(rep='2',combo='True').execute()
-                SkillCombination(direction='right',target_skills='skill_2|skill_4|skill_w|skill_s|skill_e|skill_a').execute()
+                SkillCombination(direction='right',target_skills='skill_w|skill_s|skill_e|skill_a').execute()
             
             if settings.auto_change_channel and config.should_solve_rune:
                 config.bot._solve_rune()
